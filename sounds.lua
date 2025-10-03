f36a6fa27e2ffcef524ad688b3f87da8cce7a058
maxobur0001 <msblock09@gmail.com>
2025-10-03 23:30:21 +0300

    Sounds improved

--- sounds.lua
+++ sounds.lua
@@ -4,16 +4,18 @@
 
 if SERVER then
 
-    --- Sound data
+    ---Sound data
+    ---@class Sound
+    Sound = {}
+    Sound.__index = Sound
+
+    ---Create new sound
     ---@param name string Sound name to index
     ---@param volume number Sound volume
     ---@param looping boolean Is sound looping
     ---@param url string Sound URL (I'm recomending DropBox)
-    Sound = {}
-    Sound.__index = Sound
-
     function Sound:new(name, volume, looping, url)
-        local self = setmetatable(
+        return setmetatable(
             {
                 name = name,
                 volume = volume,
@@ -22,20 +24,21 @@ if SERVER then
             },
             Sound
         )
-        return self
     end
     setmetatable(Sound, {__call = Sound.new})
 
-    function preloadSounds(...)
-        for _, snd in ipairs({...}) do
-            timer.simple(0.5, function()
-                net.start("preloadSound")
-                net.writeString(snd.url)
-                net.writeString(snd.name)
-                net.writeBool(snd.looping)
-                net.writeInt(snd.volume, 32)
-                net.send(find.allPlayers())
-            end)
+    ---Preload sounds for Astro. You should place it in ClientInitialize hook
+    ---@param ply Player Player to send sounds
+    ---@param ... Sound
+    function preloadSounds(ply, ...)
+        local sounds = {...}
+        for _, snd in ipairs(sounds) do
+            net.start("preloadSound")
+            net.writeString(snd.url)
+            net.writeString(snd.name)
+            net.writeBool(snd.looping)
+            net.writeInt(snd.volume, 32)
+            net.send(ply)
         end
     end
 
@@ -56,6 +59,8 @@ if SERVER then
     end
 
 
+    ---Stop preloaded sound
+    ---@param name string
     function stopSound(name)
         net.start("stopSound")
         net.writeString(name)
@@ -63,25 +68,94 @@ if SERVER then
     end
 
 else
-    SOUNDS = {}
-    PARENTS = {}
+    local Error = {
+        ---@type string
+        url = nil,
+        ---@type boolean
+        loop = nil,
+        ---@type number
+        volume = nil,
+        ---@type number
+        error = nil,
+        ---@type number
+        attempts = nil,
+        ---@type bool
+        in_progress = nil
+    }
+
+    function Error:new(url, loop, volume, error)
+        return setmetatable(
+            {
+                url = url,
+                loop = loop,
+                volume = volume,
+                error = error,
+                attempts = 3,
+                in_progress = false
+            },
+            Error
+        )
+    end
+
+    local SOUNDS = {}
+    local PARENTS = {}
+    local ERRORS = {}
+
+    local function message(...)
+        printConsole(Color(255, 0, 0), "[AstroSound]", Color(255, 255, 255), ...)
+    end
+
+    local function preload(url, name, loop, volume)
+        bass.loadURL(url, "3d noblock noplay", function(snd, err, errname)
+            if not snd then
+                local error = ERRORS[name]
+                message(" Sound \"" .. name .. "\" error: " .. errname .. ", skip")
+                if error then
+                    error.attempts = error.attempts - 1
+                    error.in_progress = false
+                else
+                    ERRORS[name] = Error:new(url, loop, volume, err)
+                end
+                return
+            end
+            SOUNDS[name] = snd
+            message(" Sound \"" .. name .. "\" loaded!")
+            snd:setVolume(volume)
+            snd:setLooping(loop)
+        end)
+    end
 
     net.receive("preloadSound", function()
         local url = net.readString()
         local name = net.readString()
         local loop = net.readBool()
         local volume = net.readInt(32)
-        bass.loadURL(url, "3d noblock noplay", function(snd, err, errname)
-            if not snd then
-                print(Color(255, 0, 0), "[AstroSound]", Color(255, 255, 255), " Sound \"" .. name .. "\" error: " .. errname .. ", skip")
-                return
+        preload(url, name, loop, volume)
+    end)
+
+
+    local function errorHandler()
+        while true do
+            coroutine.yield()
+            for name, err in pairs(ERRORS) do
+                if err.attempts == 0 or SOUNDS[name] then
+                    ERRORS[name] = nil
+                    continue
+                end
+                if err.in_progress then
+                    continue
+                end
+                coroutine.yield()
+                coroutine.wait(1)
+                preload(err.url, name, err.loop, err.volume)
+                err.in_progress = true
             end
-            SOUNDS[name] = snd
-            print(Color(255, 0, 0), "[AstroSound]", Color(255, 255, 255), " Sound \"" .. name .. "\" loaded!")
-            snd:setVolume(volume)
-            snd:setLooping(loop)
-            status = true
-        end)
+        end
+    end
+
+    local handlerThread = coroutine.create(errorHandler)
+    hook.add("Tick", "soundPreloadErrors", function()
+        coroutine.resume(handlerThread)
     end)
 
     local function playSound(name, pos)
