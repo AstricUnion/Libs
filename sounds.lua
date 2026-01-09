219b80ead6ba5912a0b41a95384d10f5b040ae59
maxobur0001 <msblock09@gmail.com>
2026-01-09 19:54:28 +0300

    Sounds lib edited

--- sounds.lua
+++ sounds.lua
@@ -1,62 +1,15 @@
---@name Sounds
---@author maxobur0001
---@shared
+---@name Sounds
+---@author AstricUnion
+---@shared
+
+---@class astrosounds
+local astrosounds = {}
 
 if SERVER then
-    -- Network strings --
-    -- preloadSounds
-    -- preloaded
-    -- playSound
-    -- stopSound
-
-    local astrosounds = {}
-    setmetatable(astrosounds, {__index = astrosounds})
-
-    ---Sound data
-    ---@class Sound
-    Sound = {}
-    Sound.__index = Sound
-
-    ---Create new sound
-    ---@param name string Sound name to index
-    ---@param volume number Sound volume
-    ---@param looping boolean Is sound looping
-    ---@param url string Sound URL (I'm recomending DropBox)
-    function Sound:new(name, volume, looping, url)
-        return setmetatable(
-            {
-                name = name,
-                volume = volume,
-                looping = looping or false,
-                url = url
-            },
-            Sound
-        )
-    end
-    setmetatable(Sound, {__call = Sound.new})
-
-    ---Preload sounds for Astro. You should place it in ClientInitialize hook
-    ---@param ply Player Player to send sounds
-    ---@param ... Sound
-    function astrosounds.preload(ply, ...)
-        local sounds = {...}
-        net.start("preloadSounds")
-        net.writeTable(sounds)
-        net.send(ply)
-    end
-
-
-    ---SoundPreloaded hook
-    net.receive("preloaded", function(_, ply)
-        local name = net.readString()
-        hook.run("SoundPreloaded", name, ply)
-    end)
-
-
-    ---Play preloaded sound
-    ---@param name string
-    ---@param offset Vector Offset position or parent of the sound
-    ---@param parent Entity | nil Parent of the sound
+    ---Play sound
+    ---@param name string Identifier of sound
+    ---@param offset Vector? Position or offset of this sound
+    ---@param parent Entity? Entity, parent this sound to
     ---@param plys table | Player | nil Players to send the sound
     function astrosounds.play(name, offset, parent, plys)
         net.start("playSound")
@@ -66,121 +19,86 @@ if SERVER then
         if parent then
             net.writeEntity(parent)
         end
-        net.send(plys or find.allPlayers())
+        net.send(plys)
     end
 
 
-    ---Stop preloaded sound
-    ---@param name string
+    ---Stop sound
+    ---@param name string Identifier of sound
+    ---@param plys table | Player | nil Players to stop the sound
     function astrosounds.stop(name, plys)
         net.start("stopSound")
         net.writeString(name)
-        net.send(plys or find.allPlayers())
+        net.send(plys)
     end
-
-    return astrosounds
 
 else
-    local Error = {
-        ---@type string
-        url = nil,
-        ---@type boolean
-        loop = nil,
-        ---@type number
-        volume = nil,
-        ---@type number
-        error = nil,
-        ---@type number
-        attempts = nil,
-        ---@type boolean
-        in_progress = nil
-    }
-
-    function Error:new(url, loop, volume, error)
-        return setmetatable(
-            {
-                url = url,
-                loop = loop,
-                volume = volume,
-                error = error,
-                attempts = 3,
-                in_progress = false
-            },
-            Error
-        )
-    end
-
     local SOUNDS = {}
     local PARENTS = {}
     local ERRORS = {}
 
     local function message(...)
-        printConsole(Color(255, 0, 0), "[AstroSound]", Color(255, 255, 255), ...)
+        printConsole(Color(255, 0, 0), "[AstroSound] ", Color(255, 255, 255), ...)
     end
 
-    local function preload(url, name, loop, volume)
-        bass.loadURL(url, "3d noblock noplay", function(snd, err, errname)
-            if not snd then
-                local error = ERRORS[name]
-                message(" Sound \"" .. name .. "\" error: " .. errname .. ", skip")
-                if error then
-                    error.attempts = error.attempts - 1
-                    error.in_progress = false
-                else
-                    ERRORS[name] = Error:new(url, loop, volume, err)
+    ---Preload sound
+    ---@param name string Name to identify this sound
+    ---@param volume number Volume of this sound
+    ---@param loop boolean Loop this sound
+    ---@param play boolean Play this sound after preload
+    ---@param url string URL to download sound
+    function astrosounds.preload(name, volume, loop, play, url)
+        local noplay = (!play and " noplay" or "")
+        bass.loadURL(url, "3d noblock" .. noplay, function(snd, _, errname)
+            if !snd then
+                local attempts = ERRORS[name] or 3
+                message(string.format(
+                    "Sound \"%s\" error: %s. Attempts remain: %i",
+                    name, errname, attempts
+                ))
+
+                if attempts and attempts > 0 then
+                    ERRORS[name] = attempts - 1
+                    timer.simple(1, function()
+                        astrosounds.preload(name, volume, loop, play, url)
+                    end)
                 end
                 return
             end
             SOUNDS[name] = snd
-            message(" Sound \"" .. name .. "\" loaded!")
-            net.start("preloaded")
-            net.writeString(name)
-            net.send()
+            message(string.format("Sound \"%s\" loaded!", name))
             snd:setVolume(volume)
             snd:setLooping(loop)
         end)
     end
 
-    net.receive("preloadSounds", function()
-        local sounds = net.readTable()
-        for _, snd in ipairs(sounds) do
-            preload(snd.url, snd.name, snd.looping, snd.volume)
-        end
-    end)
 
-
-    local function errorHandler()
-        while true do
-            coroutine.yield()
-            for name, err in pairs(ERRORS) do
-                if err.attempts == 0 or SOUNDS[name] then
-                    ERRORS[name] = nil
-                    continue
-                end
-                if err.in_progress then
-                    continue
-                end
-                coroutine.yield()
-                coroutine.wait(1)
-                preload(err.url, name, err.loop, err.volume)
-                err.in_progress = true
+    ---Play sound
+    ---@param name string Identifier of sound
+    ---@param offset Vector? Position or offset of this sound
+    ---@param parent Entity? Entity, parent this sound to
+    function astrosounds.play(name, offset, parent)
+        offset = offset or Vector()
+        local sound = SOUNDS[name]
+        if sound then
+            if !sound:isLooping() then
+                sound:setTime(0)
             end
+            local parentPos = (parent and parent:getPos() or Vector())
+            sound:setPos(parentPos + offset)
+            sound:play()
+            PARENTS[name] = {parent, offset}
         end
     end
 
-    local handlerThread = coroutine.create(errorHandler)
-    hook.add("Tick", "soundPreloadErrors", function()
-        coroutine.resume(handlerThread)
-    end)
 
-    local function playSound(name, pos)
+    ---Stop sound
+    ---@param name string Identifier of sound
+    function astrosounds.stop(name)
         local sound = SOUNDS[name]
         if sound then
-            if not sound:isLooping() then
-                sound:setTime(0)
-            end
-            sound:setPos(pos)
-            sound:play()
+            sound:pause()
+            sound:setTime(0)
         end
     end
 
@@ -190,29 +108,27 @@ else
         local is_parent = net.readBool()
         if is_parent then
             net.readEntity(function(ent)
-                playSound(name, ent:getPos())
-                PARENTS[name] = ent
+                astrosounds.play(name, pos, ent)
             end)
         else
-            playSound(name, pos)
+            astrosounds.play(name, pos)
         end
     end)
 
     net.receive("stopSound", function()
         local name = net.readString()
-        local sound = SOUNDS[name]
-        if sound then
-            sound:pause()
-            sound:setTime(0)
-        end
+        astrosounds.stop(name)
     end)
 
     hook.add("Think", "soundParent", function()
         for name, parent in pairs(PARENTS) do
             local snd = SOUNDS[name]
-            if snd and isValid(parent) then
-                snd:setPos(parent:getPos())
+            if snd and isValid(parent[1]) then
+                snd:setPos(parent[1]:getPos() + parent[2])
             end
         end
     end)
 end
+
+
+return astrosounds
